62b309f0405aecfe5137cc5167373db7a161fc02
WBT <WBT@users.noreply.github.com>
2022-09-06 12:20:59 -0400

    Customize static-serving source

--- source/backend/nodeServer.ts
+++ source/backend/nodeServer.ts
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 92522; //EthOnline deadline is 9/25/2022
 
-app.get('/', express.static(path.join(__dirname, 'public')));
+app.get('/', express.static(path.join(__dirname, '../frontend/public')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
