e392ef33e5852b3809ef94a2b268b88b78f9aff7
WBT <WBT@users.noreply.github.com>
2022-09-06 12:25:55 -0400

    Wrap in object

--- source/backend/nodeServer.ts
+++ source/backend/nodeServer.ts
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 92522; //EthOnline deadline is 9/25/2022
-
+export class ExpressServer {
+	constructor(port: number = 92522) { //EthOnline deadline is 9/25/2022
 app.get('/', express.static(path.join(__dirname, '../frontend/public')));
 
 app.listen(port, () => {
 	console.log(`SendCrypto server is now listening at http://localhost:${port}`);
 })
+	}
+}
+new ExpressServer();
