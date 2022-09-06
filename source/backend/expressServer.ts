3981fa1dc4cf59ff7d3b1839160706c0f948c92e
WBT <WBT@users.noreply.github.com>
2022-09-06 16:18:16 -0400

    Server main index for everything

--- source/backend/expressServer.ts
+++ source/backend/expressServer.ts
@@ -4,6 +4,7 @@ export class ExpressServer {
 	constructor(port: number = 25922) { //EthOnline deadline is 25 Sept 2022
 		app.use('/', express.static('./dist/frontend/public/images/favicons'));
 		app.use('/', express.static('./dist/frontend/public'));
+		app.get('/?*', function(req, res) {res.sendFile('./dist/frontend/public/index.html')});
 		app.listen(port, () => {
 			console.log(`SendCrypto server is now listening at http://localhost:${port}`);
 		})
