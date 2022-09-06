6eb3525e3065023ed78968845854caa126187269
WBT <WBT@users.noreply.github.com>
2022-09-06 15:34:58 -0400

    Add stub fn for addFaviconsToHead in new file

--- source/frontend/public/js/recipientPage.ts
+++ source/frontend/public/js/recipientPage.ts
@@ -1,7 +1,10 @@
 
 
+import { MostPages } from "./mostPages.js";
+
 export const RecipientPage = {
 	onPageLoad : function() {
+		MostPages.onPageLoad();
 		console.log('Scripts work but are not yet implemented!');
 	}
 }
