06929851e56ce1727eeb90bbde91b2eb87b17396
WBT <WBT@users.noreply.github.com>
2022-09-06 12:36:13 -0400

    Get port number within allowed range

--- source/backend/expressServer.ts
+++ source/backend/expressServer.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 export class ExpressServer {
-	constructor(port: number = 92522) { //EthOnline deadline is 9/25/2022
+	constructor(port: number = 25922) { //EthOnline deadline is 25 Sept 2022
 		app.get('/', express.static(path.join(__dirname, '../frontend/public')));
 		app.listen(port, () => {
 			console.log(`SendCrypto server is now listening at http://localhost:${port}`);
