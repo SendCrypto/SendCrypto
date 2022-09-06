fd37699402b8d892eb679b7402cfb3f33e01ff1a
WBT <WBT@users.noreply.github.com>
2022-09-06 15:26:06 -0400

    Restore route on /

--- source/backend/expressServer.ts
+++ source/backend/expressServer.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 export class ExpressServer {
 	constructor(port: number = 25922) { //EthOnline deadline is 25 Sept 2022
-		app.use(express.static('./dist/frontend/public'));
+		app.use('/', express.static('./dist/frontend/public'));
 		app.listen(port, () => {
 			console.log(`SendCrypto server is now listening at http://localhost:${port}`);
 		})
