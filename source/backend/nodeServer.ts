e53ddc32bc2a3af2b93805bf66e0f3823102843a
WBT <WBT@users.noreply.github.com>
2022-09-06 12:27:09 -0400

    RF: Indentation for last commit

--- source/backend/nodeServer.ts
+++ source/backend/nodeServer.ts
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 export class ExpressServer {
 	constructor(port: number = 92522) { //EthOnline deadline is 9/25/2022
-app.get('/', express.static(path.join(__dirname, '../frontend/public')));
+		app.get('/', express.static(path.join(__dirname, '../frontend/public')));
 
-app.listen(port, () => {
-	console.log(`SendCrypto server is now listening at http://localhost:${port}`);
-})
+		app.listen(port, () => {
+			console.log(`SendCrypto server is now listening at http://localhost:${port}`);
+		})
 	}
 }
 new ExpressServer();
