cc61ccff924d719056d1dcad7e34cc3c178ae611
WBT <WBT@users.noreply.github.com>
2022-09-06 12:20:48 -0400

    Adapt static file server from docs https://expressjs.com/en/starter/static-files.html

--- source/backend/nodeServer.ts
+++ source/backend/nodeServer.ts
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 92522; //EthOnline deadline is 9/25/2022
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
