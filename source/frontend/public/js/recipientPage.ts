5875d49b3e772389ecaf329eb7dc35fda3e7b9c3
WBT <WBT@users.noreply.github.com>
2022-09-06 16:12:31 -0400

    Add getParamsFromPath which does one

--- source/frontend/public/js/recipientPage.ts
+++ source/frontend/public/js/recipientPage.ts
@@ -2,9 +2,31 @@
 
 import { MostPages } from "./mostPages.js";
 
+interface MasterParams {
+	recipient ?: string,
+	amount ?: number,
+	amountCurrency ?: string,
+	sendCurrency ?: string,
+	network ?: string,
+}
+
 export const RecipientPage = {
 	onPageLoad : function() {
 		MostPages.onPageLoad();
-		console.log('Scripts work but are not yet implemented!');
-	}
+		const masterParams = RecipientPage.getParamsFromPath();
+	},
+
+	getParamsFromPath: function() : MasterParams {
+		let pathname = window.location.pathname;
+		if(pathname.startsWith('/')) { //trim if present
+			pathname = pathname.substring(1);
+		}
+		let pathParts = pathname.split('/');
+		let recipientPartIndex = 0; //may be different if subdomains are in use per #14
+		let recipient = pathParts[recipientPartIndex];
+		console.log('Recipient is ', recipient, 'pathParts: ',pathParts);
+		return {
+			recipient,
+		};
+	},
 }
