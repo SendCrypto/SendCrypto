3d22d2e538dd5d0163b523fc348c1aa2c6ac6152
WBT <WBT@users.noreply.github.com>
2022-09-06 16:20:31 -0400

    Properly join paths

--- source/backend/expressServer.ts
+++ source/backend/expressServer.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import * as path from 'path';
 const app = express();
 export class ExpressServer {
 	constructor(port: number = 25922) { //EthOnline deadline is 25 Sept 2022
 		app.use('/', express.static('./dist/frontend/public/images/favicons'));
 		app.use('/', express.static('./dist/frontend/public'));
-		app.get('/?*', function(req, res) {res.sendFile(__dirname + '../frontend/public/index.html')});
+		app.get('/?*', function(req, res) {res.sendFile(path.join(__dirname , '../frontend/public/index.html'))});
 		app.listen(port, () => {
 			console.log(`SendCrypto server is now listening at http://localhost:${port}`);
 		})
