ce8ac7e451789053a9d7a7a5858f1059ffb128b2
WBT <WBT@users.noreply.github.com>
2022-09-06 15:45:13 -0400

    Convert first link to JS addition to test

--- source/frontend/public/js/mostPages.ts
+++ source/frontend/public/js/mostPages.ts
@@ -4,9 +4,23 @@ export const MostPages = {
 	},
 
 	addFaviconsToHead: function() {
+		const head = document.getElementsByTagName('head')[0];
+		if(typeof head === 'undefined') {
+			console.error('Cannot find head element to add favicon metadata; skipping that.');
+			return;
+		}
+		head.append(
+			MostPages.createElementWithAttributes(
+				'link',
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png",
+				}
+			),
+		);
 		//Add the following, copied from realfavicongenerator.net
 		/*
-		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 		<link rel="manifest" href="/site.webmanifest">
