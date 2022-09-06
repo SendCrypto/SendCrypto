79d1a4428023374f6280c3a647063bcb69647461
WBT <WBT@users.noreply.github.com>
2022-09-06 12:21:58 -0400

    Customize startup log

--- source/backend/nodeServer.ts
+++ source/backend/nodeServer.ts
@@ -6,5 +6,5 @@ const port = 92522; //EthOnline deadline is 9/25/2022
 app.get('/', express.static(path.join(__dirname, '../frontend/public')));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`SendCrypto server is now listening at http://localhost:${port}`);
 })
