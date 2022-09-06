898adedc8d0989c3ed772a26569306fd8909d33a
WBT <WBT@users.noreply.github.com>
2022-09-06 11:12:39 -0400

    Add hello world of scripting

---
+++ source/public/js/recipientPage.ts
@@ -0,0 +1,7 @@
+
+
+export const RecipientPage = {
+	onPageLoad : function() {
+		console.log('Scripts work but are not yet implemented!');
+	}
+}
---
+++ source/public/js/sendCryptoBundle.ts
@@ -0,0 +1,3 @@
+import { RecipientPage } from "./recipientPage.js";
+
+window.addEventListener('load', function() {RecipientPage.onPageLoad();});
