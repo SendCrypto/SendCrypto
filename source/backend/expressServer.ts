ae6a15ddc9385d6b9e978bcaaddb68e313b02d4f
WBT <WBT@users.noreply.github.com>
2022-09-06 15:28:27 -0400

    Serve root from favicons too

--- source/backend/expressServer.ts
+++ source/backend/expressServer.ts
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 export class ExpressServer {
 	constructor(port: number = 25922) { //EthOnline deadline is 25 Sept 2022
+		app.use('/', express.static('./dist/frontend/public/images/favicons'));
 		app.use('/', express.static('./dist/frontend/public'));
 		app.listen(port, () => {
 			console.log(`SendCrypto server is now listening at http://localhost:${port}`);
