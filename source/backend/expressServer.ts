4c74349be657d96350a419c23749ac204b72db48
WBT <WBT@users.noreply.github.com>
2022-09-06 12:41:04 -0400

    More direct directory identification for serving

--- source/backend/expressServer.ts
+++ source/backend/expressServer.ts
@@ -1,9 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 export class ExpressServer {
 	constructor(port: number = 25922) { //EthOnline deadline is 25 Sept 2022
-		app.get('/', express.static(path.join(__dirname, '../frontend/public')));
+		app.get('/', express.static('./dist/frontend/public'));
 		app.listen(port, () => {
 			console.log(`SendCrypto server is now listening at http://localhost:${port}`);
 		})
