43dd892af23330ac7d2a9cfbd183cae37fa5f6e7
WBT <WBT@users.noreply.github.com>
2022-09-06 12:18:48 -0400

    Customize port

--- source/backend/nodeServer.ts
+++ source/backend/nodeServer.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 92522; //EthOnline deadline is 9/25/2022
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
