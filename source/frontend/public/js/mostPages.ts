386ec24a06d955159e1024f9bfc7c9aaac75bd39
WBT <WBT@users.noreply.github.com>
2022-09-06 15:48:03 -0400

    Convert 2nd icon

--- source/frontend/public/js/mostPages.ts
+++ source/frontend/public/js/mostPages.ts
@@ -18,10 +18,18 @@ export const MostPages = {
 					href: "/apple-touch-icon.png",
 				}
 			),
+			MostPages.createElementWithAttributes(
+				'link',
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png",
+				}
+			),
 		);
 		//Add the following, copied from realfavicongenerator.net
 		/*
-		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 		<link rel="manifest" href="/site.webmanifest">
 		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#66bb6a">
