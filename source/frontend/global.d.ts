772d46cba7f6ae38ef32a16898638f83dfd00091
WBT <WBT@users.noreply.github.com>
2022-09-21 18:03:43 -0400

    Add global type

---
+++ source/frontend/global.d.ts
@@ -0,0 +1,4 @@
+import type { MetaMaskInpageProvider } from '@metamask/providers';
+export declare global {
+	var ethereum: MetaMaskInpageProvider; //see https://github.com/MetaMask/metamask-docs/issues/443
+}
