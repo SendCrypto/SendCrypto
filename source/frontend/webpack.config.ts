745b423e0c7125f0655424930d7217b89c208535
WBT <WBT@users.noreply.github.com>
2022-09-21 22:36:38 -0400

    Increase webpack size warning threshold

--- source/frontend/webpack.config.ts
+++ source/frontend/webpack.config.ts
@@ -8,5 +8,11 @@ export default {
 	output: {
 		path: path.resolve(__dirname, '../../dist/frontend/public/js'),
 		publicPath: 'js/',
-	}
+	},
+	performance: {
+		//Note: It would be better to more substantively address performance issue with large bundle sizes,
+		//disabling these to get the warning back and then responding more appropriately.
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000
+	},
 }
