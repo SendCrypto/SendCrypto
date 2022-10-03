86867f231dd4b60fdd0faad593ab12ee50a96dfe
WBT <WBT@users.noreply.github.com>
2022-10-03 15:23:27 -0400

    Change local server port to 80

--- source/backend/expressServer.ts
+++ source/backend/expressServer.ts
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 export class ExpressServer {
-	constructor(port: number = 25922) { //EthOnline deadline is 25 Sept 2022
+	constructor(port: number = 80) { //Was 25922 b/c EthOnline deadline is 25 Sept 2022
 		app.use('/', express.static('./dist/frontend/public'));
 		app.get('/?*', function(req, res) {res.sendFile(path.join(__dirname , '../frontend/public/index.html'))});
 		app.listen(port, () => {
